110d00c5521dad7d2ff3d8e0889be9e2083a3535
Edoardo Tarulli <S1094923@studenti.univpm.it>
2026-02-20 17:41:07 +0100

    ultime modifiche al css per lo stile della pagina home e miglioramento stile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,18 @@
 import { Link } from 'react-router-dom'; // Import del componente Link fornito da react router dom
 import { FaUser } from 'react-icons/fa'; // Import dell'icona "persona" per l'area riservata
+import { useNavigate } from 'react-router-dom';
 import '../css/Navbar.css'; // Import del css della Navbar
 
 const Navbar = () => {
+
+    const navigatore = useNavigate()
 
     return (
 
         <div className='StileNavbar'> {/* Contenitore principale della navbar */}
             <nav className='StileElementiNav'> {/* Contenitore degli elementi della navbar e del loro stile*/}
-                <li title = "Torna alla schermata iniziale">
-                    <Link to = {'/'} style = {{textDecoration: 'none'}}> Home </Link>
-                </li> 
-                <li title = "Vai al Workflow">
-                    <Link to = {'/Workflow'} style = {{textDecoration: 'none'}}> Workflow </Link>
-                </li>
-                <li title = "Visualizza i tuoi Workflow"> I miei Workflows </li>
-
-                <button title = "Vai alla tua area riservata">
-                    <FaUser style = {{width: '20px'}}></FaUser>
-                </button>
+                <button onClick={() => navigatore('/')} title='Vai alla schermata Home' className='StileBottoneNav'><b>HOME</b></button>
+                <button onClick={() => navigatore('/Workflow')} title='Vai alla pagina Workflow' className='StileBottoneNav'><b>WORKFLOW</b></button>
             </nav>
         </div>
     )
